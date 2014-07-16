07d36fb0cfda9205b64a44e270e1f8110105ad45
SNaimath <SNaimath@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 08:05:00 +0000

    #### 1.4.7 #### * Shortcode parsing enabled in user designed templates
    git-svn-id: https://plugins.svn.wordpress.org/plugmatter-optin-feature-box-lite/trunk@949450 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plug_featurebox_lite.php
+++ plug_featurebox_lite.php
@@ -4,7 +4,7 @@ Plugin Name: Plugmatter Feature Box %NAME%
 Plugin URI: http://plugmatter.com/feature-box
 Description: Plugmatter Optin Feature Box is the Only List Building Plugin that Allows You to Create High Converting Optin Feature Boxes for Your WordPress Site.
 Author: Plugmatter
-Version: plug_featurebox_lite
+Version:  1.4.7
 Author URI: http://plugmatter.com/
 */
 
